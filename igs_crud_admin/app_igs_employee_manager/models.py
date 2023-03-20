d28412318ad5e4f43a472631cbbc0e9a96716dcf
Jefferson <lazumbra@gmail.com>
2023-03-20 13:45:20 -0300

    make email field optional

--- igs_crud_admin/app_igs_employee_manager/models.py
+++ igs_crud_admin/app_igs_employee_manager/models.py
@@ -8,7 +8,7 @@ class Employee(models.Model):
     department = models.ForeignKey(
         Department,  on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=100)
+    email = models.EmailField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.name
